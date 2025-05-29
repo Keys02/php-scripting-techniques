457d96b9bc0959ce9dcd6896e538f6eccd4078e6
Keys <keys14052002@gmail.com>
2025-05-29 11:42:42 +0000

    Use the intval function to expose values that evaluate to zero

--- zero-function.php
+++ zero-function.php
@@ -1,5 +1,5 @@
 <?php    
     function isAnEmptyNonZeroString($form_input) : bool {
-        if($form_input === '0') { return false; }
+        if(intval($form_input) === '0') { return false; }
         return empty($form_input);
     }
