dbf81b6a0525a07a5d72f472893d48bfe2bc626b
Keys <keys14052002@gmail.com>
2025-05-29 12:06:00 +0000

    Add index page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Costume Rental Company</title>
+    </head>
+    <body>
+        <form method='get' action='process.php'>
+            Product code: <input type='text' name='product-code'>
+            <br><br>
+            Price: <input type='text' name='product-price'>
+            <br><br>
+            <input type='submit'>
+        </form>
+    </body>
+</html>
